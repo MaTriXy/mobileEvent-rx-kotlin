1b86c36874b95f1abba27235d5cb01910d4c0873
shaulr <cul8erm8>
2017-08-28 08:28:50 +0300

    Added RX support to call log manager, both regular way, and experimental coroutines way

--- app/src/androidTest/java/com/tikalk/mobileevent/mobileevent/CallLogTest.kt
+++ app/src/androidTest/java/com/tikalk/mobileevent/mobileevent/CallLogTest.kt
@@ -12,9 +12,16 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 import org.junit.Rule
 import android.support.test.rule.GrantPermissionRule
+import android.text.TextUtils
+import android.util.Log
 import com.tikalk.mobileevent.mobileevent.data.ICallLogListener
+import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.runBlocking
 import java.util.*
+import java.util.concurrent.CountDownLatch
 
 
 /**
@@ -22,30 +29,31 @@ import java.util.*
  */
 @RunWith(AndroidJUnit4::class)
 class CallLogTest {
+    private val  TAG = "CallLogTest"
 
-    lateinit var context : Context
-    lateinit var manager : CallLogManager
+    lateinit var testContext: Context
+    lateinit var manager: CallLogManager
     val days = 1000 * 60 * 60 * 24
     @Rule @JvmField var permissionRule: GrantPermissionRule? =
-            GrantPermissionRule.grant(  android.Manifest.permission.WRITE_CALL_LOG,
-                                        android.Manifest.permission.READ_CALL_LOG)
+            GrantPermissionRule.grant(android.Manifest.permission.WRITE_CALL_LOG,
+                    android.Manifest.permission.READ_CALL_LOG)
 
     @Before
     fun setup() {
-        context = InstrumentationRegistry.getContext()
-        manager = CallLogManager(context)
+        testContext = InstrumentationRegistry.getContext()
+        manager = CallLogManager(testContext)
     }
 
     @Test
     fun callLogTest() {
         //test write
         val logs = ArrayList<CallLogDao>()
-        logs.add(CallLogDao(1,"123123", System.currentTimeMillis(), 1000, CallLog.Calls.OUTGOING_TYPE,true,"Test 1"))
-        logs.add(CallLogDao(2,"456456", System.currentTimeMillis(), 2000, CallLog.Calls.INCOMING_TYPE,true,"Test 2"))
-        logs.add(CallLogDao(3,"654321", System.currentTimeMillis() -  days * 5, 20000, CallLog.Calls.OUTGOING_TYPE,true,"Test 3"))
+        logs.add(CallLogDao(1, "123123", System.currentTimeMillis(), 1000, CallLog.Calls.OUTGOING_TYPE, true, "Test 1"))
+        logs.add(CallLogDao(2, "456456", System.currentTimeMillis(), 2000, CallLog.Calls.INCOMING_TYPE, true, "Test 2"))
+        logs.add(CallLogDao(3, "654321", System.currentTimeMillis() - days * 5, 20000, CallLog.Calls.OUTGOING_TYPE, true, "Test 3"))
         assertEquals("managed to write 3 records", manager.write(logs), 3)
-        assertNotNull("managed to write a record", manager.write(CallLogDao(4,"11111", System.currentTimeMillis(), 1000, CallLog.Calls.INCOMING_TYPE,true,"Test 5")))
-        assertNotNull("managed to write a record", manager.write(CallLogDao(5,"123123", System.currentTimeMillis()-  days * 10, 1000, CallLog.Calls.OUTGOING_TYPE,true,"Test 4")))
+        assertNotNull("managed to write a record", manager.write(CallLogDao(4, "11111", System.currentTimeMillis(), 1000, CallLog.Calls.INCOMING_TYPE, true, "Test 5")))
+        assertNotNull("managed to write a record", manager.write(CallLogDao(5, "123123", System.currentTimeMillis() - days * 10, 1000, CallLog.Calls.OUTGOING_TYPE, true, "Test 4")))
 
         //test read all with no selection
         assertTrue("managed to read at least two records", manager.read().size >= 3)
@@ -56,8 +64,8 @@ class CallLogTest {
         val dateSelectionArgs = manager.getDateSelectionArgs(sixDaysAgo, fourDaysAgo)
         val numberSelectionArgs = manager.getNumberSelectionArgs("123123")
 
-        assertTrue("managed to read at least one record filtered by date", manager.read(manager.SELECTION_DATE, dateSelectionArgs ).size >= 1)
-        assertTrue("managed to read at least one record filtered by number", manager.read(manager.SELECTION_NUMBER, numberSelectionArgs ).size >= 1)
+        assertTrue("managed to read at least one record filtered by date", manager.read(manager.SELECTION_DATE, dateSelectionArgs).size >= 1)
+        assertTrue("managed to read at least one record filtered by number", manager.read(manager.SELECTION_NUMBER, numberSelectionArgs).size >= 1)
 
 
         //test delete by selection
@@ -67,7 +75,7 @@ class CallLogTest {
     }
 
     @Test
-     fun testAsync() = runBlocking<Unit> {
+    fun testAsync() = runBlocking<Unit> {
         var logs = ArrayList<CallLogDao>()
         var asyncError: String? = null
         var job = manager.readAsync(object : ICallLogListener {
@@ -86,10 +94,33 @@ class CallLogTest {
                 asyncError = error
             }
         })
-        if(job != null) {
+        if (job != null) {
             job.join()
         }
         assertNull(asyncError, asyncError)
         assertTrue("Got some call logs", logs.size > 0)
+    }
+
+
+    @Test
+    fun testRx2() = runBlocking<Unit> {
+
+        val source = manager.coroutinesRxQuery(coroutineContext)
+        if (source != null) {
+            var success = false
+            source.observeOn(Schedulers.io(), false, 1) // specify buffer size of 1 item
+                    .doOnComplete {
+                        Log.d(TAG, "rx complete")
+                        success = true
+                    }
+                    .subscribe {
+                        x -> Log.d(TAG, "got " + x.toString())
+                    }
+            delay(2000)
+
+            assertTrue("got published via RX", success)
+        } else {
+            assertTrue("failed to generate RX stream", false)
+        }
     }
 }
--- app/src/main/java/com/tikalk/mobileevent/mobileevent/data/CallLogManager.kt
+++ app/src/main/java/com/tikalk/mobileevent/mobileevent/data/CallLogManager.kt
@@ -1,18 +1,27 @@
 package com.tikalk.mobileevent.mobileevent.data
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.net.Uri
 import android.provider.CallLog
 import android.support.v4.content.ContextCompat
 import android.util.Log
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.Job
-import kotlinx.coroutines.experimental.launch
-import java.util.*
+import com.squareup.sqlbrite2.SqlBrite
+import io.reactivex.Flowable
 
+import kotlinx.coroutines.experimental.*
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.experimental.reactive.publish
+
+import kotlinx.coroutines.experimental.rx2.rxFlowable
+import java.util.*
+import kotlin.coroutines.experimental.CoroutineContext
 
 
 /**
@@ -25,34 +34,35 @@ class CallLogManager(val context: Context) {
 
     val SELECTION_DATE = CallLog.Calls.DATE + " > ? and " + CallLog.Calls.DATE + " < ? "
     val SELECTION_NUMBER = CallLog.Calls.NUMBER + " = ?"
+    val sqlBrite = SqlBrite.Builder().build()
+    val resolver = sqlBrite.wrapContentProvider(context.contentResolver, Schedulers.io())
 
-
-    fun read( selection: String? = null,  selectionArgs: Array<String>? = null) : List<CallLogDao>{
+    fun read(selection: String? = null, selectionArgs: Array<String>? = null): List<CallLogDao> {
         val ret = ArrayList<CallLogDao>()
         if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CALL_LOG)) {
             val cursor = context.contentResolver.query(CallLog.Calls.CONTENT_URI, null, selection, selectionArgs, null)
-            if(!cursor.moveToFirst()) {
+            if (!cursor.moveToFirst()) {
                 return ret
             }
-            for(i in 0 .. cursor.count) {
+            for (i in 0..cursor.count) {
                 ret.add(CallLogDao(cursor))
             }
-        } else  {
+        } else {
             Log.e(TAG, "permission READ_CALL_LOG not granted! Skipping query")
         }
 
         return ret
     }
 
-    fun getDateSelectionArgs(startDate: Date, endDate: Date) : Array<String> {
+    fun getDateSelectionArgs(startDate: Date, endDate: Date): Array<String> {
         return arrayOf(startDate.time.toString(), endDate.time.toString())
     }
 
-    fun getNumberSelectionArgs(number : String) : Array<String> {
+    fun getNumberSelectionArgs(number: String): Array<String> {
         return arrayOf(number)
     }
 
-    fun deleteCallLogByNumber(number: String) : Int {
+    fun deleteCallLogByNumber(number: String): Int {
         if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CALL_LOG)) {
             return context.contentResolver.delete(CallLog.Calls.CONTENT_URI, SELECTION_NUMBER, getNumberSelectionArgs(number))
         } else {
@@ -61,7 +71,7 @@ class CallLogManager(val context: Context) {
         return -1
     }
 
-    fun deleteCallLogByDate(startDate: Date, endDate: Date) : Int {
+    fun deleteCallLogByDate(startDate: Date, endDate: Date): Int {
         if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CALL_LOG)) {
             return context.contentResolver.delete(CallLog.Calls.CONTENT_URI, SELECTION_DATE, getDateSelectionArgs(startDate, endDate))
         } else {
@@ -70,7 +80,36 @@ class CallLogManager(val context: Context) {
         return -1
     }
 
-    fun readAsync(listener: ICallLogListener,  selection: String? = null,  selectionArgs: Array<String>? = null) : Job? {
+
+    @SuppressLint("MissingPermission")
+    fun queryRx(selection: String? = null, selectionArgs: Array<String>? = null): Observable<List<CallLogDao>> {
+        return resolver.createQuery(CallLog.Calls.CONTENT_URI, null, selection, selectionArgs, null, false)
+                .mapToList({
+                    c ->
+                    CallLogDao(c)
+                }).firstElement().toObservable()
+    }
+
+    fun coroutinesRxQuery(coroutineContext: CoroutineContext, selection: String? = null, selectionArgs: Array<String>? = null): Flowable<CallLogDao>? {
+        if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CALL_LOG)) {
+            val cursor = context.contentResolver.query(CallLog.Calls.CONTENT_URI, null, selection, selectionArgs, null)
+            return coroutinesGenerateFlowable(cursor, coroutineContext)
+        }
+        return null
+    }
+
+    fun coroutinesGenerateFlowable(cursor: Cursor, coroutineContext: CoroutineContext): Flowable<CallLogDao> {
+        return rxFlowable(coroutineContext) {
+            if (cursor.moveToFirst()) {
+                do {
+                    send(CallLogDao(cursor))
+                } while (cursor.moveToNext())
+            }
+        }
+    }
+
+
+    fun readAsync(listener: ICallLogListener, selection: String? = null, selectionArgs: Array<String>? = null): Job? {
         cancelled = false
         val ret = ArrayList<CallLogDao>()
         if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CALL_LOG)) {
@@ -80,10 +119,11 @@ class CallLogManager(val context: Context) {
                     listener.onOperationStarted(ICallLogListener.Operation.read)
 
                     if (cursor.moveToFirst()) {
-                        for(i in 0 .. cursor.count/10) {
+                        for (i in 0..cursor.count / 10) {
                             for (j in 0..10) {
                                 if (cancelled) break
                                 ret.add(CallLogDao(cursor))
+                                cursor.moveToNext()
                             }
                             listener.onOperationProgress(ICallLogListener.Operation.read, ret.clone() as List<CallLogDao>)
                             ret.clear()
@@ -96,7 +136,7 @@ class CallLogManager(val context: Context) {
             } finally {
                 return job
             }
-        } else  {
+        } else {
             Log.e(TAG, "permission READ_CALL_LOG not granted! Skipping query")
             listener.onOperationError(ICallLogListener.Operation.read, "permission READ_CALL_LOG not granted! Skipping query")
             return null
@@ -129,4 +169,5 @@ class CallLogManager(val context: Context) {
         }
         return null
     }
+
 }
